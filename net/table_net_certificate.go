7dfc0d5da0f0ae7dda6368bb5b02f26aaf68c8b5
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-27 19:22:31 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#27)

--- net/table_net_certificate.go
+++ net/table_net_certificate.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/genkiroid/cert"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableNetCertificate(ctx context.Context) *plugin.Table {
--- net/table_net_dns_reverse.go
+++ net/table_net_dns_reverse.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"net"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableNetDNSReverse(ctx context.Context) *plugin.Table {
