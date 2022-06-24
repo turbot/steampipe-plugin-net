fb7d7e0fc4edb5ce55f95f8d55963772faeabd13
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2022-06-25 02:13:29 +0530

    Add table `net_http_request`. Closes #25 (#38)
    This commit also bumps `turbot/steampipe-plugin-sdk` to v3.3.0.

--- net/utils.go
+++ net/utils.go
@@ -3,11 +3,100 @@ package net
 import (
 	"context"
 	"crypto/tls"
+	"net/http"
+	"unicode/utf8"
 
 	"golang.org/x/exp/slices"
 
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 )
+
+func getQuals(qualValue *proto.QualValue) []string {
+	var data []string
+	// Check for nil
+	if qualValue == nil {
+		return data
+	}
+	if qualList := qualValue.GetListValue(); qualList != nil {
+		for _, q := range qualList.Values {
+			data = append(data, q.GetStringValue())
+		}
+	} else {
+		data = append(data, qualValue.GetStringValue())
+	}
+	return data
+}
+
+func getAuthHeaderQuals(qualValue *proto.QualValue) (authHeader string, present bool) {
+	if qualValue == nil {
+		return "", false
+	}
+	if qualList := qualValue.GetListValue(); qualList != nil {
+		for _, q := range qualList.Values {
+			return q.GetStringValue(), true
+		}
+	}
+	return qualValue.GetStringValue(), true
+}
+
+func addRequestHeaders(request *http.Request, headers map[string]interface{}) *http.Request {
+	for header, value := range headers {
+		content, isArray := value.([]interface{})
+		if isArray {
+			for _, i := range content {
+				request.Header.Add(header, i.(string))
+			}
+		} else {
+			request.Header.Add(header, value.(string))
+		}
+	}
+	return request
+}
+
+type tableNetWebRequestRow struct {
+	Url                                string
+	Method                             string
+	RequestBody                        string
+	RequestHeaders                     string
+	FollowRedirects                    bool
+	Status                             int
+	ResponseStatusCode                 int
+	ResponseHeaders                    map[string][]string
+	ResponseBody                       string
+	Error                              string
+	HeaderContentSecurityPolicy        string
+	HeaderStrictTransportSecurity      string
+	HeaderContentType                  string
+	HeaderCrossSiteScriptingProtection string
+	HeaderPermissionsPolicy            string
+	HeaderReferrerPolicy               string
+	HeaderXFrameOptions                string
+	HeaderXContentTypeOptions          string
+}
+
+type baseRequestAttributes struct {
+	Url         string
+	Methods     []string
+	RequestBody string
+	Headers     map[string]interface{}
+}
+
+func removeInvalidUTF8Char(str string) string {
+	if !utf8.ValidString(str) {
+		v := make([]rune, 0, len(str))
+		for i, r := range str {
+			if r == utf8.RuneError {
+				_, size := utf8.DecodeRuneInString(str[i:])
+				if size == 1 {
+					continue
+				}
+			}
+			v = append(v, r)
+		}
+		str = string(v)
+	}
+	return str
+}
 
 func getQualListValues(ctx context.Context, quals map[string]*proto.QualValue, qualName string) []string {
 	values := make([]string, 0)
